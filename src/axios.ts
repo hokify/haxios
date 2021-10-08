72f53c0d5d00cb445984083c303acc64fc8f509f
simon <s.tretter@gmail.com>
2021-10-08 21:16:39 +0200

    fix: type improvements and comment current implementation status

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import {GaxiosOptions, GaxiosResponse, RetryConfig} from 'gaxios'
 
-export interface HAxiosRequestConfig<D = any> extends GaxiosOptions {
+export interface HAxiosRequestConfig<D = any> extends AxiosConfig {
     data?: D;
     onDownloadProgress?: (progressEvent: any) => void;
     withCredentials?: boolean;
@@ -12,18 +12,6 @@ export interface HAxiosResponse<T = any, D = any> extends GaxiosResponse<T> {
     config: HAxiosRequestConfig<D>;
 }
 
-export type Method =
-    | 'get' | 'GET'
-    | 'delete' | 'DELETE'
-    | 'head' | 'HEAD'
-    | 'options' | 'OPTIONS'
-    | 'post' | 'POST'
-    | 'put' | 'PUT'
-    | 'patch' | 'PATCH'
-    | 'purge' | 'PURGE'
-    | 'link' | 'LINK'
-    | 'unlink' | 'UNLINK'
-
 export interface AxiosProxyConfig {
     host: string;
     port: number;
@@ -33,20 +21,18 @@ export interface AxiosProxyConfig {
     };
     protocol?: string;
 }
-export type AxiosRequestHeaders = Record<string, string>
-
 export type AxiosResponseHeaders = Record<string, string> & {
     "set-cookie"?: string[]
 }
 
+/**
 export interface AxiosRequestTransformer {
     (data: any, headers?: AxiosRequestHeaders): any;
 }
 
 export interface AxiosResponseTransformer {
     (data: any, headers?: AxiosResponseHeaders): any;
-}
-
+}*/
 
 export interface AxiosBasicCredentials {
     username: string;
@@ -63,31 +49,40 @@ export interface CancelToken {
     throwIfRequested(): void;
 }
 
-export interface AxiosConfig extends Omit<GaxiosOptions, 'method' | 'responseType' | 'validateStatus' | 'signal'> {
-    baseURL?: string;
-    transformRequest?: AxiosRequestTransformer | AxiosRequestTransformer[];
-    transformResponse?: AxiosResponseTransformer | AxiosResponseTransformer[];
-    headers?: AxiosRequestHeaders;
-    paramsSerializer?: (params: any) => string;
-    timeout?: number;
-    timeoutErrorMessage?: string;
-    withCredentials?: boolean;
-    auth?: AxiosBasicCredentials;
-    xsrfCookieName?: string;
-    xsrfHeaderName?: string;
-    onUploadProgress?: (progressEvent: any) => void;
-    onDownloadProgress?: (progressEvent: any) => void;
-    maxContentLength?: number;
-    validateStatus?: ((status: number) => boolean) | null;
-    maxBodyLength?: number;
-    maxRedirects?: number;
-    socketPath?: string | null;
-    httpAgent?: any;
-    httpsAgent?: any;
-    proxy?: AxiosProxyConfig | false;
-    cancelToken?: CancelToken;
-    decompress?: boolean;
-    signal?: AbortSignal;
+export interface AxiosConfig extends Omit<GaxiosOptions, 'baseUrl'> {
     retry?: boolean;
     retryConfig?: RetryConfig;
+    // maps to credentials: include
+    withCredentials?: boolean;
+
+
+    /* NEEDS MORE OWRK */
+    // not implemented
+    transformRequest?: never; // AxiosRequestTransformer | AxiosRequestTransformer[];
+    // not implemented
+    transformResponse?: never; // AxiosResponseTransformer | AxiosResponseTransformer[];
+    // not implemented
+    timeoutErrorMessage?: string;
+    // not implemented
+    auth?: never; // AxiosBasicCredentials;
+    // ??
+    xsrfCookieName?: string;
+    // ??
+    xsrfHeaderName?: string;
+    // not workig right now
+    onDownloadProgress?: (progressEvent: any) => void;
+    // does nothing
+    maxBodyLength?: number;
+    // not working
+    socketPath?: string | null;
+    // useless
+    httpAgent?: any;
+    // useless
+    httpsAgent?: any;
+    // accoridng to test suite not working right onw
+    proxy?: AxiosProxyConfig | false;
+    // needs also more work
+    cancelToken?: CancelToken;
+    // useless? not implenented at leat
+    decompress?: boolean;
 }
